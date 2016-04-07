67773c2166a6e6d6565693f223dbf990218b32bc
Tim Oliver <me@timoliver.com.au>
2016-04-07 12:35:44 +0800

    Added Table View Controller setup.

--- examples/ios/objc/TableViewChangeNotifications/TableViewController.m
+++ examples/ios/objc/TableViewChangeNotifications/TableViewController.m
@@ -7,8 +7,23 @@
 //
 
 #import "TableViewController.h"
+#import <Realm/Realm.h>
+
+// Realm model object
+@interface DemoObject : RLMObject
+@property NSString *title;
+@property NSDate   *date;
+@end
+
+@implementation DemoObject
+// None needed
+@end
+
+// ------------------------------
 
 @interface TableViewController ()
+
+- (void)addButtonTapped:(id)sender;
 
 @end
 
@@ -17,6 +32,42 @@
 - (void)viewDidLoad
 {
     self.title = @"ChangeNotifications";
+    
+    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addButtonTapped:)];
+    self.navigationItem.leftBarButtonItem = addButton;
+}
+
+#pragma mark - Button Callbacks -
+- (void)addButtonTapped:(id)sender
+{
+    
+}
+
+#pragma mark - Table View Data Source - 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"TableCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+        cell.accessoryType = UITableViewCellAccessoryDetailButton;
+    }
+    
+    cell.textLabel.text = @"Title";
+    cell.detailTextLabel.text = @"Subtitle";
+    
+    return cell;
+}
+
+#pragma mark - Table Delegate -
+- (void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath
+{
+    
 }
 
 @end
